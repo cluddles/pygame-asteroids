b40b690cdbe90ae1fc8cd1bf49b73331c3e87ddc
Dan Fielding <dgfielding@gmail.com>
2025-01-01 22:12:00 +0000

    Asteroid, AsteroidField

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pygame
 
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -17,8 +19,12 @@ def game_loop(screen):
     running = True
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
     while running:
         # nice if we can quit
         for event in pygame.event.get():
