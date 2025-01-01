f6c08fa886d0c5aa62c9d3692fb788b7a8154ecd
Dan Fielding <dgfielding@gmail.com>
2025-01-01 21:52:23 +0000

    Render player

--- main.py
+++ main.py
@@ -3,21 +3,22 @@
 # throughout this file
 import pygame
 
-from pygame.time import Clock
-
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = Clock()
+    clock = pygame.time.Clock()
     dt = 0
     running = True
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(FPS) / 1000
 
