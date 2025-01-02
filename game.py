be0bfb0d6ef5ac3b734442c0979db81c417faca4
Dan Fielding <dgfielding@gmail.com>
2025-01-02 10:54:13 +0000

    Separate game logic

---
+++ game.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import pygame
+
+from constants import *
+from circleshape import CircleShape
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
+class Game:
+    def __init__(self):
+        self.updatable = pygame.sprite.Group()
+        self.drawable = pygame.sprite.Group()
+        self.asteroids = pygame.sprite.Group()
+        self.shots = pygame.sprite.Group()
+
+        # This is a bit questionable; the groups are object vars but the container specs are class vars
+        Player.containers = (self.updatable, self.drawable)
+        Asteroid.containers = (self.asteroids, self.updatable, self.drawable)
+        AsteroidField.containers = (self.updatable)
+        Shot.containers = (self.updatable, self.drawable, self.shots)
+
+        self.player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        self.asteroid_field = AsteroidField()
+
+    def wrap(self, thing):
+        if not isinstance(thing, CircleShape):
+            return
+        w = SCREEN_WIDTH
+        h = SCREEN_HEIGHT
+        r = thing.radius
+        if thing.position.x + r < 0:
+            thing.position.x += w + r * 2
+        elif thing.position.x - r >= w:
+            thing.position.x -= w + r * 2
+        if thing.position.y + r < 0:
+            thing.position.y += h + r * 2
+        elif thing.position.y - r >= h:
+            thing.position.y -= h + r * 2
+
+    def update(self, dt):
+        for thing in self.updatable:
+            thing.update(dt)
+            self.wrap(thing)
+        for asteroid in self.asteroids:
+            if asteroid.is_colliding(self.player):
+                print("Game over!")
+            for shot in self.shots:
+                if asteroid.is_colliding(shot):
+                    asteroid.split()
+                    shot.kill()
+
+    def render(self, screen):
+        screen.fill((0,0,0))
+        for thing in self.drawable:
+            thing.draw(screen)
+        pygame.display.flip()
--- main.py
+++ main.py
@@ -4,70 +4,26 @@ import pygame
 
 from constants import *
 from circleshape import CircleShape
-from player import Player
-from asteroid import Asteroid
-from asteroidfield import AsteroidField
-from shot import Shot
+from game import Game
+
+def keep_running():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        return False
+    return True
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game_loop(screen)
-
-def wrap(thing, screen):
-    if not isinstance(thing, CircleShape):
-        return
-    w = screen.get_width()
-    h = screen.get_height()
-    r = thing.radius
-    if thing.position.x + r < 0:
-        thing.position.x += w + r * 2
-    elif thing.position.x - r >= w:
-        thing.position.x -= w + r * 2
-    if thing.position.y + r < 0:
-        thing.position.y += h + r * 2
-    elif thing.position.y - r >= h:
-        thing.position.y -= h + r * 2
-
-def game_loop(screen):
+    game = Game()
     clock = pygame.time.Clock()
     dt = 0
-    running = True
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-    asteroids = pygame.sprite.Group()
-    shots = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable)
-    asteroid_field = AsteroidField()
-    Shot.containers = (updatable, drawable, shots)
-    while running:
-        # nice if we can quit
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_ESCAPE]:
-            running = False
-        # logic
-        for thing in updatable:
-            thing.update(dt)
-            wrap(thing, screen)
-        for asteroid in asteroids:
-            if asteroid.is_colliding(player):
-                print("Game over!")
-                running = False
-            for shot in shots:
-                if asteroid.is_colliding(shot):
-                    asteroid.split()
-                    shot.kill()
-        # render
-        screen.fill((0,0,0))
-        for thing in drawable:
-            thing.draw(screen)
-        pygame.display.flip()
+    while (keep_running()):
+        game.update(dt)
+        game.render(screen)
         dt = clock.tick(FPS) / 1000
 
 if __name__ == "__main__":
